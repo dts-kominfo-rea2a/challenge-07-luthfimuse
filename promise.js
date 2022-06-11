b36cfe223b78feb5c23edb85b6d597aa66394854
Luthfi Burhanuddin <luthfi.burhanuddin@gmail.com>
2022-06-11 11:24:04 +0800

    challenge 07 commit

--- promise.js
+++ promise.js
@@ -1,7 +1,29 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    let ixx = await promiseTheaterIXX();
+    let vgc = await promiseTheaterVGC();
+
+    let filterHasilEmosiIxx = ixx.filter(
+      hasilSetelahMenonton => (hasilSetelahMenonton.hasil === emosi)
+    ).length
+
+    let filterHasilEmosiVgc = vgc.filter(
+      hasilSetelahMenonton => (hasilSetelahMenonton.hasil == emosi)
+    ).length
+
+    let hasil = filterHasilEmosiIxx + filterHasilEmosiVgc;
+
+    return hasil;
+
+  } catch (error) {
+    return `${error}`;
+  }
+  
+};
+
 
 module.exports = {
   promiseOutput,
